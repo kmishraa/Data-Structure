42f2a7163a3b25763e9d3703f7ff08beeb78e902
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2023-06-30 23:52:50 +0530

    Create  3_2_index_of_first_occurrence_recurssion.cpp

---
+++ 3_2_index_of_first_occurrence_recurssion.cpp
@@ -0,0 +1,37 @@
+//less time complexity will be obtained with binary search
+#include<iostream>
+using namespace std;
+int firstOcc(int arr[],int n, int x)
+{
+	int low=0,high=n-1;
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+
+		if(x>arr[mid])
+		low=mid+1;
+
+		else if(x<arr[mid])
+		high=mid-1;
+
+		else{
+			if(mid==0 || arr[mid-1]!= arr[mid])
+			// either mid=0 or element before mid is not equal to mid then return mid
+			return mid;
+
+			else
+			high= mid-1; //left part
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int arr[]={10,20,30,40,50,50,60};
+	int n=7;
+	int x=50;
+	cout<<firstOcc(arr,n,x);
+	return 0;
+
+}
