be4ee8e693e7396b34f6b9d372bc861aaff23819
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2023-07-24 23:45:56 +0530

    Create Tail_Recursion1.java

---
+++ Tail_Recursion1.java
@@ -0,0 +1,24 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+	
+	static int fact(int n, int k)
+	{
+		if(n == 0 || n == 1)
+			return k;
+
+		return fact(n - 1, k * n);
+
+	}
+    public static void main(String [] args) 
+    {
+
+    	System.out.println(fact(3, 1));
+        
+    }
+
+}
