83fd1831e0bfd7dc4b19096f7bdedd6a24b6e068
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2023-11-29 18:53:42 +0530

    Create 2_String_size.cpp

---
+++ 2_String_size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[] = "gfg";
+    cout << sizeof(str);
+    return 0;
+}
+// In C++, a character array is often terminated by a null character ('\0')
+// to mark the end of the string. So, when you declare a character array like
+// char str[] = "gfg";, it actually includes three characters ('g', 'f', 'g')
+// and an additional null character ('\0') to signify the end of the string.
