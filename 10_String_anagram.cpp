8d965aad30d28e482f2677e40bf1317d08ef716d
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2024-02-03 00:00:22 +0530

    Update 10_String_anagram.cpp

--- 10_String_anagram.cpp
+++ 10_String_anagram.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool areAnagram(string &s1,string &s2)
 {
-    int n1 = s1.lengnnth();
+    int n1 = s1.lengnth();
     int n2 = s2.length();
     if (n1 != n2)
         return false;
