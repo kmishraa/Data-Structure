40ef4bf7c998ceac4448a1f77b30b558a65d67b4
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2024-02-19 22:40:15 +0530

    Update 12_String_left_most_occurence.cpp

--- 12_String_left_most_occurence.cpp
+++ 12_String_left_most_occurence.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int leftmost(string &str)
 {
-    for (int i = 0; i < str.length(); i++)
+    for (int i = 0; i < str.length()           ; i++)
     {
         for (int j = i + 1; j < str.length(); j++)
         {
